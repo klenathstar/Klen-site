aee06cf0ec84a0b4ec0f97eab079964c45b14529
Austin Carlson <klenathstar@gmail.com>
2021-08-17 16:12:53 -0500

    Created tetris board for home page

--- pages/tetris/js/main.js
+++ pages/tetris/js/main.js
@@ -5,3 +5,8 @@ ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+function play() {
+    board = new Board(ctx);
+    console.table(board.grid);
+}
